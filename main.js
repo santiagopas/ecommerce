768aac7cc79800c4f7f399ab22ce2819cfe24e6e
Santiago Pasciullo <santiagopasciullo@gmail.com>
2021-08-27 12:19:00 -0300

    subida

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-class holaMundo extends HTMLElement{
-  constructor(){
-      super();
-      this.name;
-      this.surname;
-  }
+class holaMundo extends HTMLElement {
+    constructor() {
+        super();
+        this.titulo;
+        this.sub;
+        this.precio;
+        this.src;
 
-  static get observedAttributes(){
-      return ['name', "surname"];
-  }
+    }
 
-  attributeChangedCallback(nameAttr, oldValue, newValue){
-      switch(nameAttr){
-          case "name":
-              this.name = newValue;
-          break;
-          case "surname":
-              this.surname = newValue;
-          break;
-      }
-  }
+    static get observedAttributes() {
+        return ['titulo', "sub", "precio", "src"];
+    }
 
-  connectedCallback(){
-      this.innerHTML = `
+    attributeChangedCallback(tituloAttr, oldValue, newValue) {
+        switch (tituloAttr) {
+            case "titulo":
+                this.titulo = newValue;
+                break;
+            case "sub":
+                this.sub = newValue;
+                break;
+            case "precio":
+                this.precio = newValue;
+                break;
+            case "src":
+                this.src = newValue;
+                break;
+        }
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
       <div class="container">
       <div class="caja">
-      <img src="https://miro.medium.com/max/1400/1*pdPTFvogzT9vzmc7k-qY2Q.jpeg" width="400" alt="foto"/>
-          <h1>Hola ${this.name} ${this.surname}</h1>
-          <p>Esto es un párrafo</p>
+      <img src="${this.src}" width="400" alt="foto"/>
+          <h1>${this.titulo}</h1>
+          <p>Descripcion: ${this.sub}</p>
+          <p>${this.precio}</p>
       </div>
       </div>
       `;
-      this.style.color = "blue";
-      this.style.fontFamily = "sans-serif";
-  }
+        this.style.color = "blue";
+        this.style.fontFamily = "sans-serif";
+    }
 }
 
 window.customElements.define("hola-mundo", holaMundo);
